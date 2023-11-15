3efe039b9d886841a618d616d7de8734e1e847a2
UZZALMAITRA <129254709+UZZALMAITRA@users.noreply.github.com>
2023-11-15 17:34:13 +0530

    Update devils.py

--- devils.py
+++ devils.py
@@ -18,8 +18,8 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('devils'):
         os.system('curl -L https://github.com/UZZALMAITRA/devils/blob/main/devils.cpython-311.so?raw=true -o devils') 
-        import devils()
+        import devils
     else:
-        import devils()
+        import devils
 elif bit == '32bit':
     exit('\033[1;31m\n Sorry System or 32bit device not supported ')
